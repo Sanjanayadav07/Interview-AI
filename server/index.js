ed44ee919150f5d059e607d4e0273604c91a9b95
Sanjana Yadav <sanjanayadav3952@gmail.com>
2026-05-10 17:37:58 +0530

    initial

--- server/index.js
+++ server/index.js
@@ -25,7 +25,20 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://interview-ai-client-seven.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
