1f604f679e1d496a6bc767c8f6ff5abc4ac55d12
Sanjana Yadav <sanjanayadav3952@gmail.com>
2026-05-10 17:32:07 +0530

    initial

--- server/index.js
+++ server/index.js
@@ -15,15 +15,17 @@ console.log("STRIPE SECRET =>", process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 
-app.use(cors({
+const corsOptions = {
   origin: [
     "http://localhost:5173",
     "https://interview-ai-client-seven.vercel.app"
   ],
-  credentials: true
-}));
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+};
 
-app.options("*", cors());
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
